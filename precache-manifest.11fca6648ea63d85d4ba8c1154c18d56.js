6684a9d07dc966630a08ea150c7af2cbef59076a
Loops <danys2809@gmail.com>
2019-07-01 12:47:25 -0500

    Updates

---
+++ precache-manifest.11fca6648ea63d85d4ba8c1154c18d56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd14144e7746dad513de9558eee6e91",
+    "url": "/DanyLoops/SalesDashboard/index.html"
+  },
+  {
+    "revision": "1a19a7af6e341e125fcb",
+    "url": "/DanyLoops/SalesDashboard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/DanyLoops/SalesDashboard/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1a19a7af6e341e125fcb",
+    "url": "/DanyLoops/SalesDashboard/static/js/main.218f88ec.chunk.js"
+  },
+  {
+    "revision": "8db816d0e4bdc76ba50d",
+    "url": "/DanyLoops/SalesDashboard/static/js/runtime~main.ac322f8c.js"
+  }
+]);
